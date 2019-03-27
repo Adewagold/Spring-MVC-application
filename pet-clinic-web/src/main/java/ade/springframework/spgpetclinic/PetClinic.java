d2e7cb7a817e43c0775b8aea15c6f53559df1b65
Adewale Adeleye <adewale.adeleye@interswitchgroup.com>
2019-03-27 09:18:42 +0000

    Adding Index page and controller. Closes #8

---
+++ pet-clinic-web/src/main/java/ade/springframework/spgpetclinic/PetClinic.java
@@ -0,0 +1,14 @@
+package ade.springframework.spgpetclinic;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by adewale adeleye on 26/03/2019
+ **/
+@SpringBootApplication
+public class PetClinic {
+    public static void main(String[] args)
+    {
+
+    }
+}
--- pet-clinic-web/src/main/java/ade/springframework/spgpetclinic/SpgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/ade/springframework/spgpetclinic/SpgPetClinicApplication.java
@@ -1,13 +1,14 @@
 package ade.springframework.spgpetclinic;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SpgPetClinicApplication {
 
     public static void main(String[] args) {
-//        ApplicationContext ctx = SpringApplication.run(ade.springframework.spgpetclinic.SpgPetClinicApplication.class, args);
-//        MyController controller = (MyController) ctx.getBean("myController");
+        SpringApplication.run(ade.springframework.spgpetclinic.SpgPetClinicApplication.class, args);
+      //  MyController controller = (MyController) ctx.getBean("myController");
 //        controller.hello();
 //        System.out.println(ctx.getBean(PropertyInjectionController.class).sayHello());
 //        System.out.println(ctx.getBean(SetterInjection.class).sayHello());
